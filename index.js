e25751c391aaca7cf762f547dc72c032b0e7009a
shaath404 <84157430+shaath404@users.noreply.github.com>
2021-07-02 11:59:54 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -25,15 +25,26 @@ function readUrl() {
 
 function generateMeme(){
 
+    window.scrollTo(0,0);
+
     html2canvas(document.querySelector("#theUserMeme")).then(canvas => {
         // document.body.appendChild(canvas)
-        var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        var image = canvas.toDataURL('image/jpeg', 1);
         console.log(image);
-        window.location.href = image;
+        // window.open(image);
+
+        debugBase64(image);
+        
     });
 
 
 }
+
+function debugBase64(base64URL){
+    var win = window.open();
+    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+}
+
 
 function changeMemeText(){
 
