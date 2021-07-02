708ea3c175616da301d06d9b6073d526bb420f70
shaath404 <84157430+shaath404@users.noreply.github.com>
2021-07-02 12:19:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ function generateMeme(){
 
 }
 
-function debugBase64(base64URL){
-    var win = window.open();
-    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
-}
 
 
 function changeMemeText(){
